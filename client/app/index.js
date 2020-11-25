7f129e12725355e0717b97e3e121c57e4cd27f9d
daniel camucatto <daniel.camucatto@gmail.com>
2020-11-25 15:17:14 -0300

    add POO

--- client/app/index.js
+++ client/app/index.js
@@ -1,16 +1,30 @@
-let fields = [
-    document.querySelector('#data'),
-    document.querySelector('#value'),
-    document.querySelector('#quantity')
-];
-console.log(fields);
-let tbody = document.querySelector('table tbody');
-console.log(tbody); 
-document.querySelector('.form').addEventListener('submit', event =>{
-    let tr = document.createElement('tr');
-    fields.forEach(function(fields){
-        let td = document.createElement('td');
-        td.textContent = fields.value;
-        tr.appendChild(td);
-    })
-})
+// let fields = [
+//     document.querySelector('#data'),
+//     document.querySelector('#value'),
+//     document.querySelector('#quantity')
+// ];
+// console.log(fields);
+
+// let tbody = document.querySelector('table tbody');
+// console.log(tbody); 
+// document.querySelector('.form').addEventListener('submit', event =>{
+//     event.preventDefault();
+//     let tr = document.createElement('tr');
+//     fields.forEach(function(fields){
+//         let td = document.createElement('td');
+//         td.textContent = fields.value;
+//         tr.appendChild(td);
+//     })
+//     let tdVolume = document.createElement('td')
+//     tdVolume.textContent = fields[1].value * fields[2].value;
+//     tr.appendChild(tdVolume);
+
+//     tbody.appendChild(tr);
+//     // limpar fields
+//     fields[0].value = '';
+//     fields[1].value = '';
+//     fields[2].value = '';
+    
+//     // foca n campo da data 
+//     fields[0].focus();
+// })
